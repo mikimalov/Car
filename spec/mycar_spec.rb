86021c08af343825e1c456330b3eecad8e21ecf4
Miki <miki.stomatologija@gmail.com>
2020-03-03 23:53:04 +0100

    Speeding up

--- spec/mycar_spec.rb
+++ spec/mycar_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe Car::MyCar do
         audi = Car::MyCar.new('audi', 'black', 2018)
         expect(audi.speed).to eq(0)
     end
+
+    it "increases current speed for 40 km/h" do
+        audi = Car::MyCar.new("audi", "black", 2018) 
+        expect(audi.speed_up).to be(40)
+    end
 end
+
